4c3fcc8dec4629c3a883c4497b61f162403f896d
Adrien Bougouin <adrien.bougouin@gmail.com>
2018-05-05 12:40:12 +0900

    update main

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,13 @@ use omoikane::regression::LinearRegressionModel;
 fn main() {
     let mut model = LinearRegressionModel::new();
 
-    // f(x) = x
+    // f(x) = 2x
     model.fit_supervised_dataset(&vec!((vector!(1.0), 2.0),
                                        (vector!(2.0), 4.0),
                                        (vector!(3.0), 6.0),
                                        (vector!(4.0), 8.0),
                                        (vector!(5.0), 10.0),
                                        (vector!(6.0), 12.0)));
-    // f(1.5) = 1.5
+    // f(1.5) = 3
     println!("{}", model.predict(&vector!(1.5)));
 }
